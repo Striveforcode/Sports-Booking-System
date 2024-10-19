3e839b1a6a4649c40d9cb0ed13316ee109185243
Striveforcode <striveforcode@gmail.com>
2024-10-20 02:07:56 +0530

    Update admin.py

--- turf/admin.py
+++ turf/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Contact, TurfBooking, TurfBooked, BookSlot, Time  # Import the Time model as well
 
 # Register your models here.
+
+from .models import *
+# Register your models here.
+
+
 admin.site.register(Contact)
-admin.site.register(TurfBooking)  # Updated from turfBooking to TurfBooking
 admin.site.register(TurfBooked)
-admin.site.register(BookSlot)      # Make sure this matches the model name (BookSlot)
-admin.site.register(Time)          # Don't forget to register the Time model
+admin.site.register(bookslot)
